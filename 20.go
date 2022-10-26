3d42685a1322384ed9c74d03ad8c026d429664b9
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2022-10-26 15:11:30 +0200

    add 20. Valid Parentheses

---
+++ 20.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	// "unicode/utf8"
+)
+
+// https://www.educative.io/answers/how-to-implement-a-stack-in-golang
+
+type Stack []int32
+
+func (s *Stack) Len() int {
+	return len(*s)
+}
+
+func (s *Stack) Push(el int32) {
+	*s = append(*s, el)
+}
+
+func (s *Stack) Pop() (int32, bool) {
+	s_len := s.Len()
+
+	if s_len == 0 {
+		return 0, false
+	}
+
+	index := s_len - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element, true
+}
+
+func (s *Stack) Last() (int32, bool) {
+	s_len := s.Len()
+	if s_len == 0 {
+		return 0, false
+	}
+	index := s_len - 1
+	element := (*s)[index]
+	return element, true
+}
+
+func isValid(s string) bool {
+
+	var stack Stack
+
+    // fmt.Println(s)
+
+	for _, el := range s {
+
+		el_from_stack, check := stack.Last()
+
+		good := false
+
+		if check {
+			if (int32('(') == el_from_stack) && (int32(')') == el) {
+				_, _ = stack.Pop()
+				good = true
+			} else {
+				if (int32('[') == el_from_stack) && (int32(']') == el) {
+					_, _ = stack.Pop()
+					good = true
+				} else {
+					if (int32('{') == el_from_stack) && (int32('}') == el) {
+						_, _ = stack.Pop()
+						good = true
+					}
+				}
+			}
+		}
+
+		if !good {
+			stack.Push(el)
+		}
+
+		// fmt.Printf("%d - %c \n", i, el)
+	}
+
+	return stack.Len() == 0
+}
+
+func main() {
+	fmt.Println(isValid("{{{{{}}}}}}"))
+}
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Some of my tasks
 | 9. Palindrome Number | https://leetcode.com/problems/palindrome-number | 
 | 13. Roman to Integer | https://leetcode.com/problems/roman-to-integer | 
 | 14. Longest Common Prefix | https://leetcode.com/problems/longest-common-prefix | 
+| 20. Valid Parentheses | https://leetcode.com/problems/valid-parentheses | 
 | 23. Merge k Sorted Lists | https://leetcode.com/problems/merge-k-sorted-lists |
 | 42. Trapping Rain Water | https://leetcode.com/problems/trapping-rain-water | 
 | 407. Trapping Rain Water II | https://leetcode.com/problems/trapping-rain-water-ii | 

cc758cf6bc68e8933c167c24ae3e3631371251a8
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2022-12-27 14:26:18 +0200

    fix 168. Excel Sheet Column Title

--- 168.py
+++ 168.py
@@ -12,21 +12,24 @@
 # ------------------------------ #  
 
 import string
+
 class Solution:
     def __init__(self):
         self.STR_ASCII = [*string.ascii_uppercase]
         self.LEN_ASCII = len(self.STR_ASCII)
+        # print(self.STR_ASCII)
     def convertToTitle(self, columnNumber: int) -> str:
         num = columnNumber
         res = []
         while True:
-            res.append(self.STR_ASCII[(num % self.LEN_ASCII) - 1])
+            # print("num = ", num)
+            res.append(self.STR_ASCII[num % self.LEN_ASCII - 1])
             if num <= self.LEN_ASCII:
                 break
-            num //= self.LEN_ASCII 
+            num = (num - 1) // self.LEN_ASCII
         return "".join(res[::-1])
 
 if __name__ == '__main__':
-    print(Solution().convertToTitle(51))
+    print(Solution().convertToTitle(701)) #52 = AZ,  701 = ZY
     print('ok')
 

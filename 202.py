48418510027eec2dd13018d8be12c79dd1725243
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-01-06 20:49:47 +0200

    fix 202. Happy Number

--- 202.py
+++ 202.py
@@ -17,14 +17,25 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         digits = n
         sm_q = .0
+        memory = {}
         while True:
             num = digits % 10
-            print(num)
+            # print(num)
             sm_q += num * num
+            # print("sm_q = ", sm_q)
             if digits < 10:
-                break
-            digits = math.trunc(digits / 10.0)
-        return True    
+                if sm_q == 0:
+                    return False
+                if sm_q == 1:
+                    return True
+                if sm_q in memory:
+                    return False
+                memory[sm_q] = True
+                digits = sm_q
+                sm_q = 0
+            else:    
+                digits = math.trunc(digits / 10.0)
+        return False   
 
 if __name__ == '__main__':
     print(Solution().isHappy(19))

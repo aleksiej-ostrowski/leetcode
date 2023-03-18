1df084bb594283eed5dedcbc444d1728aea6acba
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-03-18 14:07:59 +0100

    448. Find All Numbers Disappeared in an Array

---
+++ 448.go
@@ -0,0 +1,41 @@
+/*
+
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2023      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #
+
+*/
+
+package main
+
+import (
+    "fmt"
+)
+
+
+func findDisappearedNumbers(nums []int) []int {
+    memory := make(map[uint32]bool)
+    max_el := len(nums)
+    for _, el := range(nums) {
+        memory[uint32(el)] = true
+    }
+    res := []int{}
+    for el:=1;el<=max_el;el++ {
+        if _, ok := memory[uint32(el)]; !ok {
+           res = append(res, el)
+        }
+    }
+    return res
+}
+
+
+func main() {
+    res := findDisappearedNumbers([]int{4,3,2,7,8,2,3,1})
+    fmt.Println(res) 
+}

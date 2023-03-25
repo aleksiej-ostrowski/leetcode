49878c55bb164925830c7765b13556d701dadbae
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-03-25 15:13:38 +0100

    add SQL schemies for several files

--- 176.sql
+++ 176.sql
@@ -1,3 +1,10 @@
+Create table If Not Exists Employee (id int, salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+
+
 SELECT max(salary) SecondHighestSalary 
 FROM Employee 
 WHERE salary < (SELECT max(salary) FROM Employee)
--- 177.sql
+++ 177.sql
@@ -1,3 +1,10 @@
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   DECLARE OFS INT;

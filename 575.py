936e4c8756b3c70cdbaa04a7d27e3fa1104ffb89
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-04-22 10:56:36 +0200

    add pass to 575

---
+++ 575.py
@@ -0,0 +1,23 @@
+""" 575.py """
+
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2023      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #  
+
+from typing import List
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().distributeCandies([1,1,2,2,3,3]))
+    print('ok')
+

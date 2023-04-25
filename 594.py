bd99d180d4098f2c0726554faf0813845e076415
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-04-25 23:37:14 +0200

    new 594.py

---
+++ 594.py
@@ -0,0 +1,22 @@
+""" 594.py """
+
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2023      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #  
+
+from typing import List
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        pass
+
+if __name__ == '__main__':
+    print(Solution.findLHS(nums=[1,3,2,2,5,2,3,7]))
+    print('ok')
+

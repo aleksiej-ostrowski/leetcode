998ca5a9bc8ec101143a8e768fa83d93d47365dc
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-10-25 20:21:52 +0300

    add 671. Second Minimum Node In a Binary Tree

---
+++ 671.go
@@ -0,0 +1,72 @@
+/*
+
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2023      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #
+
+*/
+
+package main
+
+import (
+	"fmt"
+    "sort"
+)
+
+/**
+ * Definition for a binary tree node.
+**/ 
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func findSecondMinimumValue(root *TreeNode) int {
+    keys := make(map[int]int)
+    findSecondMinimumValue_help(root, keys)
+    res := make([]int, 0, len(keys))
+	for key := range keys {
+		res = append(res, key)
+	}
+    keys = nil
+    sort.Ints(res) 
+    x0 := res[0]
+    for _, el := range res[1:] {
+        if x0 != el {
+            return el
+        }    
+    }
+    return -1
+}
+
+func findSecondMinimumValue_help(root *TreeNode, keys map[int]int) {
+
+    if root == nil {
+        return
+    }
+
+    keys[root.Val] = 1
+
+    findSecondMinimumValue_help(root.Left, keys)
+    findSecondMinimumValue_help(root.Right, keys)
+}
+
+func main() {
+    tree3 := TreeNode{5, nil, nil}
+    tree4 := TreeNode{7, nil, nil}
+    tree2 := TreeNode{5, &tree3, &tree4}
+    tree1 := TreeNode{2, nil, nil}
+    tree0 := TreeNode{2, &tree1, &tree2}
+
+    fmt.Println(findSecondMinimumValue(&tree0))
+
+	fmt.Println("ok")
+}
--- README.md
+++ README.md
@@ -140,6 +140,7 @@ Some of my tasks
 | 653. Two Sum IV - Input is a BST | https://leetcode.com/problems/two-sum-iv-input-is-a-bst | 
 | 657. Robot Return to Origin | https://leetcode.com/problems/robot-return-to-origin | 
 | 661. Image Smoother | https://leetcode.com/problems/image-smoother | 
+| 671. Second Minimum Node In a Binary Tree | https://leetcode.com/problems/second-minimum-node-in-a-binary-tree | 
 | 1114. Print in Order | https://leetcode.com/problems/print-in-orde |
 | 1115. Print FooBar Alternately | https://leetcode.com/problems/print-foobar-alternately |
 | 1622. Fancy Sequence | https://leetcode.com/problems/fancy-sequence |

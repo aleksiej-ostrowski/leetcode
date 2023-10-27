715d4e3c96fe18f0ecdba780e6bc6e6d5aa4ad3b
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-10-27 13:16:57 +0300

    correct 674.py

--- 674.py
+++ 674.py
@@ -13,22 +13,27 @@
 
 from typing import List
 
-class Solution:
-    def findLengthOfLCIS(self, nums: List[int]) -> int:
-        gl_sm = -1
-        for ind, el in enumerate(nums):
-            sm = 1
-            last = el
-            for ind, el2 in enumerate(nums[ind+1:]):
-                if last < el2:
-                    sm += 1
-                    last = el2
-                else:
-                    break
-            if sm > 0 and sm > gl_sm:
-                gl_sm = sm
-        return gl_sm        
-
+class Solution:    
+    def findLengthOfLCIS(self, nums: List[int]) -> int:    
+        gl_sm = -1  
+        ind = 0  
+        while True:    
+            if ind >= len(nums):
+                break
+            sm = 1    
+            last = nums[ind]    
+            for ind2 in range(ind+1, len(nums)):    
+                el2 = nums[ind2]
+                if last < el2:    
+                    sm += 1    
+                    last = el2    
+                else: 
+                    ind = ind2 - 1   
+                    break    
+            if sm > 0 and sm > gl_sm:    
+                gl_sm = sm    
+            ind += 1    
+        return gl_sm  
 
 if __name__ == '__main__':
     # print(Solution().findLengthOfLCIS([1,3,5,4,7]))
---
+++ 680.go
@@ -0,0 +1,28 @@
+
+/*
+
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2023      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func validPalindrome(s string) bool {
+    
+}
+
+func main() {
+
+}

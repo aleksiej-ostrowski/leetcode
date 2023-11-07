4eda3941f08138faa5162d7355c3dda4a1c1d85b
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2023-11-07 23:58:58 +0300

    add 680. Valid Palindrome II

--- 671.go
+++ 671.go
@@ -16,57 +16,57 @@ package main
 
 import (
 	"fmt"
-    "sort"
+	"sort"
 )
 
 /**
  * Definition for a binary tree node.
-**/ 
+**/
 
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 func findSecondMinimumValue(root *TreeNode) int {
-    keys := make(map[int]int)
-    findSecondMinimumValue_help(root, keys)
-    res := make([]int, 0, len(keys))
+	keys := make(map[int]int)
+	findSecondMinimumValue_help(root, keys)
+	res := make([]int, 0, len(keys))
 	for key := range keys {
 		res = append(res, key)
 	}
-    keys = nil
-    sort.Ints(res) 
-    x0 := res[0]
-    for _, el := range res[1:] {
-        if x0 != el {
-            return el
-        }    
-    }
-    return -1
+	keys = nil
+	sort.Ints(res)
+	x0 := res[0]
+	for _, el := range res[1:] {
+		if x0 != el {
+			return el
+		}
+	}
+	return -1
 }
 
 func findSecondMinimumValue_help(root *TreeNode, keys map[int]int) {
 
-    if root == nil {
-        return
-    }
+	if root == nil {
+		return
+	}
 
-    keys[root.Val] = 1
+	keys[root.Val] = 1
 
-    findSecondMinimumValue_help(root.Left, keys)
-    findSecondMinimumValue_help(root.Right, keys)
+	findSecondMinimumValue_help(root.Left, keys)
+	findSecondMinimumValue_help(root.Right, keys)
 }
 
 func main() {
-    tree3 := TreeNode{5, nil, nil}
-    tree4 := TreeNode{7, nil, nil}
-    tree2 := TreeNode{5, &tree3, &tree4}
-    tree1 := TreeNode{2, nil, nil}
-    tree0 := TreeNode{2, &tree1, &tree2}
+	tree3 := TreeNode{5, nil, nil}
+	tree4 := TreeNode{7, nil, nil}
+	tree2 := TreeNode{5, &tree3, &tree4}
+	tree1 := TreeNode{2, nil, nil}
+	tree0 := TreeNode{2, &tree1, &tree2}
 
-    fmt.Println(findSecondMinimumValue(&tree0))
+	fmt.Println(findSecondMinimumValue(&tree0))
 
 	fmt.Println("ok")
 }
